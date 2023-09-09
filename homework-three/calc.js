355cf3c547a243a16834ff7b9167ffe6692a219e
braynwash <f.tzgeraldz@gmail.com>
2023-09-09 17:50:22 -0400

    Fixed JS result sentence

--- homework-three/calc.js
+++ homework-three/calc.js
@@ -4,6 +4,7 @@ function calcInterest() {
     let t =  parseFloat(document.getElementById('time').value);
     var interestRate= p*r*t/100;
     var total=p+interestRate;
-    document.getElementById('answer').innerHTML = "With a beginning principle of $" + p + " " + "and with a growth rate of " + r + "% for " + t + " " + "years, your total interest will be $" + interestRate + " " + "with a grand total of $" + total + ". ";
+    var answer=total.toFixed(2)
+    document.getElementById('answer').innerHTML = "With a beginning principle of $" + p + " " + "and with a growth rate of " + r + "% for " + t + " " + "years, your total interest will be $" + interestRate + " " + "with a grand total of $" + answer + ". ";
 }
 

f9623029e732d6a90fbc812e62b256bb6dab1528
braynwash <f.tzgeraldz@gmail.com>
2023-11-27 19:44:09 -0500

    Added show functionality

--- homework-ten/enhance.js
+++ homework-ten/enhance.js
@@ -1,11 +1,26 @@
+var pass = document.getElementById("pass");
 var passForm = document.forms[0];
-
 var passBtn = passForm.getElementsByTagName("img")[0];
 
 passBtn.addEventListener("click", showPassword);
 
 function showPassword() {
   var imgSrc = passBtn.getAttribute("src");
-  console.log("Clicked!");
   var hidePassImg = imgSrc.indexOf("hide");
+  console.log(hidePassImg);
+  hidePassImg = hidePassImg < 0;
+  if ((hidePassImg = true)) {
+    //if hide img is present
+    document;
+    passBtn.setAttribute(
+      "src",
+      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/27019/showPassIcon.png"
+    );
+    document.getElementById("pass").setAttribute("type", "text");
+  } else {
+    passBtn.setAttribute(
+      "src",
+      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/27019/hidePassIcon.png"
+    );
+  }
 }

f667adcc351b1f4aa3671086f26968d5521bba48
braynwash <f.tzgeraldz@gmail.com>
2023-12-02 14:44:52 -0500

    Completed coin flip

---
+++ final/coinFlip/coin.js
@@ -0,0 +1,28 @@
+var coinImg = document.getElementsByTagName("img")[0]; //selects img
+var coinResult = document.getElementById("coinResult"); //selects result
+var resultTable = document.getElementsByTagName("table")[0]; //selects table
+
+let flips = 0; //sets initial flips to 0
+
+headsTails = () => {
+  flips += 1; //increases flips each time button is clicked
+  let headsTails = Math.floor(Math.random() * 2) + 1;
+  //   console.log(headsTails);
+  if (headsTails == 1) {
+    //heads is 1
+    var result = "Heads";
+    coinImg.setAttribute("src", "images/heads.jpg");
+  } else {
+    //tails is 2
+    var result = "Tails";
+    coinImg.setAttribute("src", "images/tails.png");
+  }
+  coinResult.innerText = `You got ${result}!`;
+  console.log(flips);
+  resultTable.innerHTML += `
+  <tr>
+  <td>${flips}</td> 
+  <td>${result}</td>
+  </tr>`;
+  console.log(result);
+};

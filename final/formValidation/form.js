f24d8973c7ed2493120431e31f537f8ad2c84360
braynwash <f.tzgeraldz@gmail.com>
2023-12-07 01:08:47 -0500

    Added input DOM selectors and final links page

--- final/formValidation/form.js
+++ final/formValidation/form.js
@@ -8,12 +8,45 @@
 // Credit card must consist of 12 numbers
 // After the user clicks the submit button, all the fields are validated. If there are any errors, the errors will appear next to the respective input field.
 
+//---input ids---
+//account
+var email = document.getElementById("email");
+var pw = document.getElementById("pw");
+var confirm = document.getElementById("confirm");
+//contact
+var fName = document.getElementById("fName");
+var lName = document.getElementById("lName");
+var email = document.getElementById("email");
+var address = document.getElementById("address");
+var city = document.getElementById("city");
+var state = document.getElementById("state");
+var zip = document.getElementById("zip");
+var phone = document.getElementById("phone");
+//payment
+var type = document.getElementById("type");
+var cardNum = document.getElementById("cardNum");
+var exp = document.getElementById("exp");
+
+//---error ids---
+//account
+var emailError = document.getElementById("emailError");
+var pwError = document.getElementById("pwError");
+var confirmError = document.getElementById("confirmError");
+//contact
+var fError = document.getElementById("fError");
+var lError = document.getElementById("lError");
+var addError = document.getElementById("addressError");
+var cityError = document.getElementById("cityError");
+var stateError = document.getElementById("stateError");
+var zipError = document.getElementById("zipError");
+var phoneError = document.getElementById("phoneError");
+//payment
+var typeError = document.getElementById("typeError");
+var cardError = document.getElementById("cardError");
+var expError = document.getElementById("expError");
+
 validForm = () => {
   console.log("Validate");
-};
-
-resetForm = () => {
-  console.log("Reset");
 };
 
 document
@@ -22,8 +55,3 @@ document
     event.preventDefault();
     validForm();
   });
-
-document.getElementById("reset").addEventListener("click", function (event) {
-  event.preventDefault();
-  resetForm();
-});

d8c1571efe68fbff9c572ba7b4a3eb1cb68a854d
braynwash <f.tzgeraldz@gmail.com>
2023-12-08 01:37:32 -0500

    Added empty input checks

--- final/formValidation/form.js
+++ final/formValidation/form.js
@@ -46,7 +46,139 @@ var cardError = document.getElementById("cardError");
 var expError = document.getElementById("expError");
 
 validForm = () => {
-  console.log("Validate");
+  let validForm = true;
+  //---account---
+  //email
+  if (email.value == "") {
+    emailError.innerText = "Please fill out this field!";
+    emailError.classList.add("error");
+    validForm = false;
+  } else {
+    emailError.innerText = " ";
+    emailError.classList.remove("error");
+  }
+  //pw
+  if (pw.value == "") {
+    pwError.innerText = "Please fill out this field!";
+    pwError.classList.add("error");
+    validForm = false;
+  } else {
+    pwError.innerText = " ";
+    pwError.classList.remove("error");
+  }
+  //confirm
+  if (confirm.value == "") {
+    confirmError.innerText = "Please fill out this field!";
+    confirmError.classList.add("error");
+    validForm = false;
+  } else {
+    confirmError.innerText = " ";
+    confirmError.classList.remove("error");
+  }
+  //---contact----
+  //first name
+  if (fName.value == "") {
+    fError.innerText = "Please fill out this field!";
+    fError.classList.add("error");
+    validForm = false;
+  } else {
+    fError.innerText = " ";
+    fError.classList.remove("error");
+  }
+  //last name
+  if (lName.value == "") {
+    lError.innerText = "Please fill out this field!";
+    lError.classList.add("error");
+    validForm = false;
+  } else {
+    lError.innerText = " ";
+    lError.classList.remove("error");
+  }
+  //address
+  if (address.value == "") {
+    addError.innerText = "Please fill out this field!";
+    addError.classList.add("error");
+    validForm = false;
+  } else {
+    addError.innerText = " ";
+    addError.classList.remove("error");
+  }
+  //city
+  if (city.value == "") {
+    cityError.innerText = "Please fill out this field!";
+    cityError.classList.add("error");
+    validForm = false;
+  } else {
+    cityError.innerText = " ";
+    cityError.classList.remove("error");
+  }
+  //state
+  if (state.value == "") {
+    stateError.innerText = "Please fill out this field!";
+    stateError.classList.add("error");
+    validForm = false;
+  } else {
+    stateError.innerText = " ";
+    stateError.classList.remove("error");
+  }
+  //zip
+  if (zip.value == "") {
+    zipError.innerText = "Please fill out this field!";
+    zipError.classList.add("error");
+    validForm = false;
+  } else {
+    zipError.innerText = " ";
+    zipError.classList.remove("error");
+  }
+  //phone
+  if (phone.value == "") {
+    phoneError.innerText = "Please fill out this field!";
+    phoneError.classList.add("error");
+    validForm = false;
+  } else {
+    phoneError.innerText = " ";
+    phoneError.classList.remove("error");
+  }
+  //---payment---
+  //type
+  if (type.value == "empty") {
+    typeError.innerText = "Please select a card type!";
+    typeError.classList.add("error");
+    validForm = false;
+  } else {
+    typeError.innerText = " ";
+    typeError.classList.remove("error");
+  }
+  //cardNum
+  if (cardNum.value == "") {
+    cardError.innerText = "Please fill out this field!";
+    cardError.classList.add("error");
+    validForm = false;
+  } else {
+    cardError.innerText = " ";
+    cardError.classList.remove("error");
+  }
+  //exp
+  if (exp.value == "") {
+    expError.innerText = "Please fill out this field!";
+    expError.classList.add("error");
+    validForm = false;
+  } else {
+    expError.innerText = " ";
+    expError.classList.remove("error");
+  }
+  //if validForm is...
+  if (!validForm) {
+    document.getElementById(
+      "regComplete"
+    ).innerText = `One or more fields are incomplete. Please try again!`;
+    document.getElementById("regComplete").classList.add("error");
+  } else {
+    document.getElementById(
+      "regComplete"
+    ).innerText = `You have successfully registered!`;
+    document.getElementById("regComplete").classList.remove("error");
+  }
 };
 
 document
